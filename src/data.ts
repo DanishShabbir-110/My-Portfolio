df102a9b7de3e0792b3276e9da88bf9dbfdf6aac
DanishShabbir-110 <danishshabbir110@gmail.com>
2026-06-28 22:55:11 +0500

    Profile

--- src/data.ts
+++ src/data.ts
@@ -6,7 +6,7 @@ export const resumeData = {
     location: "Rawalpindi,Pakistan",
     email: "danishshabbir110@gmail.com",
     phone: "0302-0058872",
-    photo: "/danish.jpg",
+    photo: "/profile.jpeg",
     resume: "/resume.pdf",
     links: [
       "www.linkedin.com/in/danishshabbir110",
